96c2dc55ae46905662c3ef49e591bc17357c6df4
apurvD <86904145+apurvD@users.noreply.github.com>
2025-03-25 16:08:11 -0400

    Added Signup file and components folder

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+const Footer=()=>{
+    return(
+        <div className="footer">
+            <h3>
+                E-commerce Dashboard
+            </h3>
+        </div>
+    )
+}
+
+export default Footer;
